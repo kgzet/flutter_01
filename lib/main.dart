4ff4231107c991d5649752c83a96469b1ad06b9d
kgzet <164067705+kgzet@users.noreply.github.com>
2024-08-29 19:51:32 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'screens/home.dart';
 
-// Dla takiego małego projektu możesz zrobić "generowanie" tej listy w MyAppState
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Article list',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      child: MaterialApp(
+        title: 'Article list',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+          scaffoldBackgroundColor: Colors.amberAccent[100],
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
       ),
-      // home: MyHomePage(title: 'List of articles'),
-      // home: MainPage(),
-      home: const HomePage(),
     );
   }
 }
