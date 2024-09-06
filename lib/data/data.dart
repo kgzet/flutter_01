6afb5b1f865cbc5b4a97dd674fae2fbf091bfa93
kgzet <164067705+kgzet@users.noreply.github.com>
2024-09-06 23:30:01 +0200

    Update data.dart

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,5 +1,12 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../models/articles.dart';
 
+
+IconData iconBack = Icons.arrow_back;
+
 final List<ArticleList> articlesList = ArticleList.getArticles();
+
+DateTime now = DateTime.now();
+String formattedDate = DateFormat('kk:mm:ss - EEE d MMM').format(now);
