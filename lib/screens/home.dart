78514e13687bc49229931eb6c715c8b77d745b09
kgzet <164067705+kgzet@users.noreply.github.com>
2024-09-06 23:33:23 +0200

    custom appbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:intl/intl.dart';
 
-// import '../models/articles.dart';
-import '../data/data.dart' as data;
+import '../data/data.dart';
+import '../models/custom_app_bar.dart';
+import '../models/right_drawer.dart';
 import 'article_view.dart';
 
 
@@ -13,102 +16,119 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final theme = Theme.of(context);
-    final styleAppbarTitle = theme.textTheme.displayMedium!.copyWith(
-      fontSize: 28.sp,
-      color: theme.colorScheme.onPrimary,
-      fontWeight: FontWeight.bold,
-    );
+    // final styleAppbarTitle = theme.textTheme.displayMedium!.copyWith(
+      // fontSize: 28,
+      // color: theme.colorScheme.onPrimary,
+      // fontWeight: FontWeight.bold,
+    // );
 
     final styleTitle = theme.textTheme.displayMedium!.copyWith(
-      fontSize: 24.sp,
-      color: theme.colorScheme.onPrimary,
+      fontSize: 22.sp,
+      // color: theme.colorScheme.onPrimary,
       fontWeight: FontWeight.bold,
     );
     final styleSubtitle = theme.textTheme.displayMedium!.copyWith(
       fontSize: 15.sp,
-      color: theme.colorScheme.onPrimary,
+      // color: theme.colorScheme.onPrimary,
       fontWeight: FontWeight.normal,
     );
     final styleDate = theme.textTheme.displayMedium!.copyWith(
       fontSize: 15.sp,
-      color: Colors.red,
+      // color: Colors.red,
+      color: Theme.of(context).colorScheme.tertiary,
       fontWeight: FontWeight.bold,
     );
 
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
-          title: const Text("List of articles"),
-          centerTitle: true,
-          titleTextStyle: styleAppbarTitle,
-          elevation: 2.0,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(MediaQuery.of(context).size.height / 8),
+          child: const CustomAppBar(title: 'List of articles'),
         ),
-        body:
-          GridView.builder(
-            itemCount: data.articlesList.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10.0.w,
-              mainAxisSpacing: 10.0.w,
-              childAspectRatio: 0.7,
-            ),
-            itemBuilder: (buider, index){
-              return
-              GestureDetector(
+        // appBar: AppBar(
+        //   // backgroundColor: Colors.amberAccent,
+        //   title: const Text("List of articles"),
+        //   centerTitle: true,
+        //   titleTextStyle: styleAppbarTitle,
+        //   elevation: 2.0,
+        //   // formattedDate
+        // ),
+        // appBar: MyAppBar(appTitle: 1),
+        endDrawer: const MyDrawer(aa:1),
+        body: ListView.separated(
+            padding: const EdgeInsets.only(top: 15,),
+            itemCount: articlesList.length,
+            separatorBuilder: (context, index) => const SizedBox(height: 15,),
+            itemBuilder: (context, index) {
+              return GestureDetector(
                 onTap: (){
                   Navigator.push(context, MaterialPageRoute(
                     builder: (context) => ArticlePage(pageIndex: index)
-                  ));
-                  },
-                child:Card(
-                  elevation: 15,
-                  color: theme.colorScheme.primary,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0), 
-                    child: Column(
-                      children: [
-                        Flexible(
-                          flex: 5,
-                          fit: FlexFit.tight,
-                          child: Padding(
-                            padding: EdgeInsets.only(top: 10.0.h, right: 10.0.w, left: 10.0.w),
-                            child: Image.network(data.articlesList[index].articleImage),
-                          ),
+                    ));},
+                child: Center( 
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: 500.0,),
+                    child: Card(
+                      elevation: 15,
+                      // color: theme.colorScheme.primary,
+                      // color: theme.colorScheme.onSurface,
+                      child: Padding(
+                        // padding: EdgeInsets.symmetric(horizontal: 12.0.w, vertical: 8.0.h),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children:[
+                            Flexible(
+                              flex: 2,
+                              fit: FlexFit.tight,
+                              child: Container(
+                                margin: const EdgeInsets.only(left: 10.0),
+                                child: Image.network(articlesList[index].articleImage, fit: BoxFit.contain,)),
+                            ),
+                            const SizedBox(
+                              width: 12.0,
+                            ),
+                            Flexible(
+                              flex: 3,
+                              fit: FlexFit.tight,
+                              child: Column(
+                                children: [
+                                  // Text(
+                                  AutoSizeText(
+                                    minFontSize: 25,
+                                    maxFontSize: 45,
+                                    '${articlesList[index].articleTitle.first} ${articlesList[index].articleTitle.second}',
+                                    style: styleTitle,
+                                    semanticsLabel: "${articlesList[index].articleTitle.first} ${articlesList[index].articleTitle.second}",
+                                  ),
+                                  // Text(
+                                  AutoSizeText(
+                                    minFontSize: 15,
+                                    maxFontSize: 28,
+                                    "${articlesList[index].articleSubtitle}...",
+                                    style: styleSubtitle, 
+                                  ),
+                                  // Text(
+                                  AutoSizeText(
+                                    minFontSize: 15,
+                                    maxFontSize: 28,
+                                    // "${articlesList[index].articleDate.day}-${articlesList[index].articleDate.month}-${articlesList[index].articleDate.year}",
+                                    DateFormat('dd-MM-yyyy').format(articlesList[index].articleDate),
+                                    style: styleDate,
+                                  ),
+                                ],
+                              ),
+                            )
+                          ]
                         ),
-                        Flexible(
-                          flex: 2,
-                          fit: FlexFit.tight,
-                          child: Text(
-                            '${data.articlesList[index].articleTitle.first} ${data.articlesList[index].articleTitle.second}',
-                            style: styleTitle,
-                            semanticsLabel: "${data.articlesList[index].articleTitle.first} ${data.articlesList[index].articleTitle.second}",
-                          ),
-                        ),
-                        Flexible(
-                          flex: 3,
-                          fit: FlexFit.tight,
-                          child: Text(
-                            "${data.articlesList[index].articleSubtitle}...",
-                            style: styleSubtitle, 
-                          ),
-                        ),
-                        Flexible(
-                          flex: 1,
-                          fit: FlexFit.tight,
-                          child: Text(
-                            "${data.articlesList[index].articleDate.day}-${data.articlesList[index].articleDate.month}-${data.articlesList[index].articleDate.year}",
-                            style: styleDate,
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
                 ),
               );
-            }
-          )
-        ),
-      );
+            },
+          ),
+      ),
+    );
   }
 }
