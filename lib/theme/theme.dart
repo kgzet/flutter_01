fd3e4759cfce3ec87f2700cb965ad80a833646e2
kgzet <164067705+kgzet@users.noreply.github.com>
2024-09-12 17:21:01 +0200

    cleaning the code

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 ThemeData lightMode = ThemeData(
   useMaterial3: true,
   brightness: Brightness.light,
   colorSchemeSeed: Colors.amber,
-  // scaffoldBackgroundColor: Theme.of(context).colorScheme.surface,
+
 );
 
 ThemeData darkMode = ThemeData(
